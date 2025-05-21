e13110b0b0ec7a9e79e0b132765f9b8000e221a2
M-yoshimura-ML <masadrst@gmail.com>
2025-05-21 15:44:38 +0900

    User Register page & register function

---
+++ actions/auth.actions.ts
@@ -0,0 +1,7 @@
+'use server';
+
+import { register } from "@/backend/controllers/auth.controller";
+
+export async function registerUser(name: string, email: string, password: string) {
+    return await register(name, email, password);
+}
