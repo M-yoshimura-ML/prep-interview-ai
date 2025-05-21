32b615ac159b9f1bfa94d9613f84b34ab986ed59
M-yoshimura-ML <masadrst@gmail.com>
2025-05-21 14:51:16 +0900

    remove commentout

--- backend/controllers/auth.controller.ts
+++ backend/controllers/auth.controller.ts
@@ -19,35 +19,5 @@ export const register = async (name: string, email: string, password: string) =>
     return newUser?._id ? {created: true} : (() => {
         throw new Error("User not created");
     })();
-}
-
-// export const register = async (req: NextApiRequest, res: NextApiResponse) => {
-//     const { email, password, name } = req.body;
-
-//     if (!email || !password || !name) {
-//         return res.status(400).json({ message: "All fields are required" });
-//     }
-
-//     try {
-//         const existingUser = await User.findOne({ email });
-
-//         if (existingUser) {
-//             return res.status(400).json({ message: "User already exists" });
-//         }
-
-//         const hashedPassword = await bcrypt.hash(password, 10);
-
-//         const newUser = new User({
-//             email,
-//             password: hashedPassword,
-//             name,
-//         });
-
-//         await newUser.save();
-
-//         return res.status(201).json({ message: "User registered successfully" });
-//     } catch (error) {
-//         console.error(error);
-//         return res.status(500).json({ message: "Internal server error" });
-//     }
-// }
+};
+          
