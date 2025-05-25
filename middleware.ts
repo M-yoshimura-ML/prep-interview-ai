0567936d19d58154d949e094743bb09b4f68d375
M-yoshimura-ML <masadrst@gmail.com>
2025-05-25 13:59:27 +0900

    protect route from unauthentication users

---
+++ middleware.ts
@@ -0,0 +1,12 @@
+import withAuth from "next-auth/middleware";
+import { IUser } from "./backend/models/user.model";
+
+export default withAuth(function middleware(req) {
+    const url = req.nextUrl?.pathname;
+    const user = req.nextauth?.token?.user as IUser;
+})
+
+export const config = {
+    // Define the paths that should be protected by authentication
+    matcher: ["/app/:path*"],
+};
