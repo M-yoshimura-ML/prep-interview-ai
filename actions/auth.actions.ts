60390b47e6864a967143db9a26a5bae3cbcc04cc
M-yoshimura-ML <masadrst@gmail.com>
2025-05-25 21:37:22 +0900

    setup cloudinary & update profile

--- actions/auth.actions.ts
+++ actions/auth.actions.ts
@@ -1,7 +1,21 @@
 'use server';
 
-import { register } from "@/backend/controllers/auth.controller";
+import { register, updateUserProfile } from "@/backend/controllers/auth.controller";
 
 export async function registerUser(name: string, email: string, password: string) {
     return await register(name, email, password);
+}
+
+export async function updateProfile({
+    name,
+    email,
+    avatar,
+    oldAvatar
+}: {
+    name: string;
+    email: string;
+    avatar?: string;
+    oldAvatar?: string;
+}) {
+    return await updateUserProfile({name, userEmail: email, avatar, oldAvatar});
 }
--- backend/controllers/auth.controller.ts
+++ backend/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import dbConnect from "../config/dbConnect";
 import { catchAsyncErrors } from "../middlewares/catchAsyncErrors";
 import User from "../models/user.model";
+import { delete_file, upload_file } from "../utils/cloudinary";
 
 export const register = catchAsyncErrors(async (name: string, email: string, password: string) => {
     await dbConnect();
@@ -23,4 +24,30 @@ export const register = catchAsyncErrors(async (name: string, email: string, pas
         throw new Error("User not created");
     })();
 });
-          
+
+
+export const updateUserProfile = catchAsyncErrors(
+    async ({
+        name,
+        userEmail,
+        avatar,
+        oldAvatar
+    }: {name: string, userEmail: string, avatar?: string, oldAvatar?: string}) => {
+        await dbConnect();
+
+        const data: {
+            name: string,
+            profilePicture?: {id: string, url: string }
+        } = {name}
+
+        if(avatar) {
+            data.profilePicture = await upload_file(avatar, "prepai/avatars")
+        }
+        if(oldAvatar) {
+            await delete_file(oldAvatar);
+        }
+
+        await User.findOneAndUpdate({email: userEmail}, {...data});
+        return {updated: true}
+    }
+);
