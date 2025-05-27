cd59b4642eb59fcab03646adf1790cb530a3ae5a
M-yoshimura-ML <masadrst@gmail.com>
2025-05-27 10:25:54 +0900

    Add AppSidebar & icon

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,10 +1,11 @@
+import AppSiderbar from '@/components/layout/sidebar/AppSidebar'
 import React from 'react'
 
 const AppLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className='grid grid-cols-1 md:grid-cols-12 gap-4 md:gap-10'>
         <div className="col-span-1 md:col-span-4 lg-col-span-3">
-            <div>Sidebar</div>
+            <AppSiderbar />
         </div>
 
         <div className="col-span-1 md:col-span-8 lg-col-span-9">
