183944e970ff106360ce38935b5dca732d36c8ff
M-yoshimura-ML <masadrst@gmail.com>
2025-06-01 21:06:23 +0900

    Result page & caclucate result score

--- app/app/results/page.tsx
+++ app/app/results/page.tsx
@@ -1,12 +1,37 @@
-'use client';
+import React from 'react';
+import { cookies } from 'next/headers';
+import { getAuthHeader } from '@/helpers/auth';
+import ListResults from '@/components/result/ListResults';
 
-import Results from '@/components/result/Results';
-import React from 'react'
 
-const ResultPage = () => {
-  return (
-    <Results />
-  )
+async function getInterviews() {
+    try {
+        const nextCookies = await cookies();
+        const authHeader = getAuthHeader(nextCookies);
+
+        const res = await fetch(`${process.env?.API_URL}/api/interviews`, {
+            headers: authHeader.headers,
+            method: 'GET',
+        });
+        
+        if (!res.ok) {
+            throw new Error('Failed to fetch interviews');
+        }
+
+        const data = await res.json();
+        
+        return data;
+    } catch (error: any) {
+        throw new Error(`Error fetching interviews: ${error?.message}`);
+    }
 }
 
-export default ResultPage;
+const ResultsPage = async () => {
+    const data = await getInterviews();
+
+    return (
+        <ListResults data={data} />
+    )
+}
+
+export default ResultsPage;
