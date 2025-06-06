c1cad83ba40cfdfd8587b7fb57a2ec14487f8b1b
M-yoshimura-ML <masahiro.yoshimura@monstar-lab.com>
2025-06-06 12:23:21 +0800

    subscribe frontend form

---
+++ app/subscribe/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import Subscribe from '@/components/payment/Subscribe'
+import React from 'react'
+
+const SubscribePage = () => {
+    return (
+        <Subscribe />
+    )
+}
+
+export default SubscribePage
