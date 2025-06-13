fbeb96e7e0eb9b1d5c6dd7010bf47bb39fefed0e
M-yoshimura-ML <masadrst@gmail.com>
2025-06-13 12:11:56 +0900

    404 NotFound page

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+import NotFound from '@/components/layout/not-found/NotFound'
+import React from 'react'
+
+const NotFoundPage = () => {
+  return (
+    <NotFound />
+  )
+}
+
+export default NotFoundPage
